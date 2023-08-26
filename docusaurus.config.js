b5def40f876366a11207417ae87deb829eee01f3
swebdev <swebdev@recocards.com>
2023-08-26 09:12:47 -0700

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/sanjay51/recocards-help',
+            'https://github.com/sanjay51/recocards-help/tree/main/',
         },
         //blog: false,
         blog: {
